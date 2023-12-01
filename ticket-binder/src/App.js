64007895d918dc6e1b423896809e48295bdb67bd
Jack Whisler <whisler1@gmail.com>
2023-12-01 17:38:33 -0600

    generic api call

--- ticket-binder/src/App.js
+++ ticket-binder/src/App.js
@@ -1,23 +1,37 @@
 import logo from './logo.svg';
 import './App.css';
+import React, { useState, useEffect } from 'react';
 
 function App() {
+  const [data, setData] = useState(null);
+  const apiKey = process.env.SETLIST_API_KEY;
+
+  useEffect(() => {
+    const fetchData = async () => {
+      try {
+        const response = await fetch('https://api.example.com/data');
+        const result = await response.json();
+        setData(result);
+      } catch (error) {
+        console.error('Error fetching data:', error);
+      }
+    };
+
+    fetchData();
+  }, []);
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+         Setlist Sandbox
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+        <div>
+      {data ? (
+        <p>Data: {JSON.stringify(data)}</p>
+      ) : (
+        <p>Loading...</p>
+      )}
+        </div>
     </div>
   );
 }
